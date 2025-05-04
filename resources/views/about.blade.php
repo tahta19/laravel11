1ffbdebf489aa0f959bb9070a6e74cb988f2ba73
tahta19 <tahtashine19@gmail.com>
2025-05-04 17:55:18 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman About</title>
+</head>
+<body>
+  <h1> Halaman About </h1>
+  <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est quaerat reprehenderit illo omnis error numquam facilis praesentium doloremque expedita nemo, aliquid perferendis rem distinctio alias, ratione ut nulla nesciunt repudiandae cupiditate deleniti! Rerum architecto repellat quaerat, fuga animi illo quas illum ipsam qui obcaecati, et quos ullam commodi quidem. Facere!
+  </h1>
+</body>
+</html>
